ecdc5eb7115ac45f96ec4e60a917b3bdadccd626
aigor0863 <aigor0863@gmail.com>
2025-03-31 13:36:48 +0000

    finalizacao do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import iconStar from "./assets/icon-star.svg"
-import illustrationThankYou from "./assets/illustration-Thank-You.svg"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 
 
@@ -41,10 +41,16 @@ export function App() {
         <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1" onClick={handleSubmite}>Submit</button>
       </div>
       ) : (
-        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
-        <img src={illustratioThankYou} alt="thank you" />
+        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="thank you" />
 
-        <p></p>
+        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You Selected {rateNote} out of 5!</p>
+
+        <h1 className="font-bold text-2xl mb-2.5">
+          Thank You!</h1>
+
+        <p className="text-light-grey text-sm leading-1 ">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+
         </div>
       )
    
